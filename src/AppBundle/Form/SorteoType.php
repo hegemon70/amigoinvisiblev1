74d22af895c6adb15708bba3851afacd78048753
Fernando Méndiz Carpi <hegemon70@gmail.com>
2017-07-30 19:36:23 +0200

    creados creados automaticamente los formularios de las entidades

---
+++ src/AppBundle/Form/SorteoType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SorteoType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('codigoSorteo')->add('mensaje')->add('asunto');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Sorteo'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_sorteo';
+    }
+
+
+}
