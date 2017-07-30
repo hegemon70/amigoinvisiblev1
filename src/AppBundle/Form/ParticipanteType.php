442976f71886ca3d536f82dfebcbd74fd8c5df3d
Fernando Méndiz Carpi <hegemon70@gmail.com>
2017-07-30 19:44:13 +0200

    sustituyo los form de ParticipanteType y SorteoType dejando sin tocar el SesionType (que no creo que vaya a usar)

--- src/AppBundle/Form/ParticipanteType.php
+++ src/AppBundle/Form/ParticipanteType.php
@@ -5,6 +5,12 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use AppBundle\Entity\Participante;
+
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class ParticipanteType extends AbstractType
 {
@@ -13,7 +19,13 @@ class ParticipanteType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('nombre')->add('correo')->add('asignado')->add('idSorteo');
+        $builder->add('nombre',TextType::class,array('attr' => array('class'=>'form-control','placeholder'=>'Nombre')))
+                ->add('correo',TextType::class,array('attr' => array('class'=>'form-control','placeholder'=>'email')))
+                ->add('asignado',HiddenType::class)
+                ->add('sorteo',HiddenType::class)
+                ->add('save', SubmitType::class, array('label'=> 'Guerdar','attr'=>array('class'=>'btn btn-default')))
+                -> add('cancel', SubmitType::class, array('label'=>'Cnacelar','attr'=>array('formnovalidate'=>'formnovalidate','class'=>'btn btn-default')))
+                ;
     }
     
     /**
@@ -22,7 +34,7 @@ class ParticipanteType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'AppBundle\Entity\Participante'
+            'data_class' => 'AppBundle\Entity\participante'
         ));
     }
 
--- src/AppBundle/Form/SorteoType.php
+++ src/AppBundle/Form/SorteoType.php
@@ -6,6 +6,12 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
 class SorteoType extends AbstractType
 {
     /**
@@ -13,7 +19,12 @@ class SorteoType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('codigoSorteo')->add('mensaje')->add('asunto');
+       // $builder->add('codigoSorteo',TextType::class,array('attr' => array('class'=>'form-control','disabled'=>'disabled')))
+        $builder->add('codigoSorteo',TextType::class,array('attr' => array('class'=>'form-control')))
+                ->add('mensaje',TextareaType::class,array('attr' => array('class'=>'form-control','placeholder'=>'Escribe aqui las condiciones del sorteo, el precio maximo del regalo ,la fecha limite')))
+                ->add('asunto',TextType::class,array('attr' => array('class'=>'form-control focusedInput','placeholder'=>'escribe aqui un titulo o asunto para el Sorteo del Amigo Invisible')))
+                ->add('save', SubmitType::class, array('label' => 'Guardar', 'attr'=>array('class'=>'btn btn-default')))
+                 -> add('cancel', SubmitType::class, array('label'=>'Cancelar','attr'=>array('formnovalidate'=>'formnovalidate','class'=>'btn btn-default')));
     }
     
     /**
